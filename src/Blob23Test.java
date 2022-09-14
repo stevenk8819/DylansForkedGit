da262d5ae8a9e5e83687952f96b87bf8383f9d3f
Andrew <andrewgtz099@gmail.com>
2022-09-14 14:57:04 -0700

    Blob works and most of Index

---
+++ src/Blob23Test.java
@@ -0,0 +1,91 @@
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class Blob23Test {
+
+	@BeforeAll
+	public static void setUpBeforeClass() throws Exception {
+		new File("Objects").mkdir();
+		File tester = new File("blobTest.txt");
+		BufferedWriter writeInTest = new BufferedWriter(new FileWriter(tester));
+		writeInTest.write("This is the test");
+		writeInTest.close();
+		
+	}
+
+	@AfterAll
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testBlob23() throws Exception {
+		//fail("Not yet implemented");
+		
+	}
+	
+	public static String getSHA1(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+
+	    MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+	    crypt.reset();
+	    crypt.update(password.getBytes("UTF-8"));
+
+	    return new BigInteger(1, crypt.digest()).toString(16);
+	}
+
+	@Test
+	public void testShaTheFile() throws NoSuchAlgorithmException, IOException {
+		//fail("Not yet implemented");
+		boolean check = false;
+		Blob23 blob = new Blob23("blobTest.txt");
+		String str = "";
+		str = blob.getShawedString();
+		String test = getSHA1(blob.getFileContents());
+		//System.out.println(str.length());
+		if(str.equals(test)) {
+			check = true;
+			assertTrue(check);
+		}
+		
+	}
+
+	@Test
+	public void testCreateTheNewSha1File() throws Exception {
+		//fail("Not yet implemented");
+		setUpBeforeClass();
+		boolean test = false;
+		Blob23 blob = new Blob23("blobTest.txt");
+		blob.shaTheFile();
+		blob.createTheNewSha1File();
+		String str = blob.getShawedString();
+		File file = new File("objects/" + str + ".txt");
+		file.createNewFile();
+		if(file.exists()) {
+			test = true;
+			assertTrue(test);
+		}
+
+		
+		
+	}
+
+}
--- src/IndexTest.java
+++ src/IndexTest.java
@@ -1,22 +1,35 @@
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.*;   // instead of  import org.junit.Test;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 
 public class IndexTest {
 
-	@BeforeClass
+	@BeforeAll
 	public static void setUpBeforeClass() throws Exception {
-		
+	
+		File testFile = new File("testFile.txt");
+		FileWriter writeInTest = new FileWriter(testFile);
+		writeInTest.write("This is the test");
 	}
 
-	@AfterClass
+	@AfterAll
 	public static void tearDownAfterClass() throws Exception {
+		File deleteFile = new File("testFile.txt");
+		deleteFile.delete();
 	}
 
 	@Test
@@ -26,20 +39,49 @@ public class IndexTest {
 		assertTrue(test.exists());
 		
 	}
+	
+	
 
 	@Test
-	public void testAddBlob() {
-		fail("Not yet implemented");
+	public void testAddBlob() throws Exception {
+		setUpBeforeClass();
+		boolean checker = false;
+		//fail("Not yet implemented");
+		String fileName = "testFile.txt";
+		Blob23 blobby = new Blob23(fileName);
+		Index test = new Index();
+		test.addBlob(fileName);
+		String str = "";
+		BufferedReader br = new BufferedReader(new FileReader("index"));
+		while(br.ready()) {
+			if(br.readLine().equals(fileName + " : " + blobby.getShawedString())) {
+				checker = true;
+				
+			}
+			
+		}
+		assertTrue(checker);
+		tearDownAfterClass();
+		
+		
 	}
 
 	@Test
 	public void testRemoveBlob() {
 		fail("Not yet implemented");
+		
 	}
 
 	@Test
-	public void testClearIndexFile() {
-		fail("Not yet implemented");
+	public void testClearIndexFile() throws IOException {
+		//fail("Not yet implemented");
+		boolean check = false;
+		BufferedReader br = new BufferedReader(new FileReader("index"));
+		if(!br.ready()) {
+			check = true;
+			assertTrue(check);
+		}
+		
 	}
 
 	@Test
