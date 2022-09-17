e52b41df28de919fd6a78d93baf4064ae5f839f7
DylanArdizzone <dardizzone1@hwemail.com>
2022-09-17 13:19:28 -0700

    doubly linked list class created

--- src/Commit.java
+++ src/Commit.java
@@ -1,4 +1,37 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Commit {
-
+	private DoublyLinkedList theList;
+	private String pTree;
+	private String summary;
+	private String author;
+	private String date;
+	private String shawed;
+	
+	public Commit(Commit parent, String toBecomePTree, String toSummary, String toAuthor) throws NoSuchAlgorithmException {
+		theList = new DoublyLinkedList();
+		pTree = toBecomePTree;
+		summary = toSummary;
+		author = toAuthor;
+		shawed = makeHash();
+	}
+	
+	public String makeHash() throws NoSuchAlgorithmException {
+		return GenerateHash(pTree + summary);
+	}
+	
+    public String GenerateHash(String input) throws NoSuchAlgorithmException {
+        MessageDigest objSHA = MessageDigest.getInstance("SHA-1");
+        byte[] bytSHA = objSHA.digest(input.getBytes());
+        BigInteger intNumber = new BigInteger(1, bytSHA);
+        String strHashCode = intNumber.toString(16);
+		
+        // pad with 0 if the hexa digits are less then 40.
+        while (strHashCode.length() < 40) {
+            strHashCode = "0" + strHashCode;
+        }
+        return strHashCode;
+    }
 }
